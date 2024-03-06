20ffbb3e06a1e5ebc5f12a1c80bb7614d50e6100
Luis Silva <luisfvieirasilva@gmail.com>
2024-03-06 10:04:10 -0300

    chore: project setup
    Project already with Spring Boot and DGS

---
+++ src/main/kotlin/com/example/socialTimeline/SocialTimelineApplication.kt
@@ -0,0 +1,11 @@
+package com.example.socialTimeline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialTimelineApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialTimelineApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/socialTimeline/SocialTimelineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.socialTimeline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialTimelineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
