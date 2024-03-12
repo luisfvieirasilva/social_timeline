7f33ff324ea66158eb42910d2ba579012d0a7426
Luis Silva <luisfvieirasilva@gmail.com>
2024-03-12 16:25:42 -0300

    chore: remove unnecessary UserReactionRelationshipRepository

--- src/main/kotlin/com/example/socialTimeline/db/repositories/UserReactionRelationshipRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.socialTimeline.db.repositories
-
-import com.example.socialTimeline.db.entities.UserReactionRelationship
-import org.springframework.data.repository.CrudRepository
-
-interface UserReactionRelationshipRepository : CrudRepository<UserReactionRelationship, String>
